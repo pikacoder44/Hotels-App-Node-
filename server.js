b12025c6871ac0258249c0782a22241f815eccca
Syed Hashir Ali <pikaop125@gmail.com>
2025-04-27 00:22:39 +0500

    Linked to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the restraunt. How may I help you?");
@@ -16,4 +17,5 @@ const menuRoutes = require("./routes/menuRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", menuRoutes);
 
-app.listen(3000);
+
+app.listen(PORT);
